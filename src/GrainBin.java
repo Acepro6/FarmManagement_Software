1baf1830393a12541024339c40557b08826a0d66
Acepro6 <artfritzke@gmail.com>
2022-04-10 13:23:37 -0600

    Started working on and debugging GrainBin.java class Summary: Changed addGrain for excess grain left from after dump Changed removeGrain for when the requested grain is more than existing grain in bin Added test cases for all methods in different situations Updated documentation

--- src/GrainBin.java
+++ src/GrainBin.java
@@ -2,15 +2,17 @@
  *  Created by:     Arthur Fritzke
  *  Date:           March 28th, 2022
  *  Last Author:    Arthur Fritzke
- *  Last Modified:  March 28th, 2022
+ *  Last Modified:  April 10th, 2022
  */
+
+import java.awt.*;
 
 public class GrainBin implements StorageUnit{
     // Maximum amount of bushels to be stored
     int capacity;
 
     // Current amount being stored
-    int current;
+    int current = 0;
 
     // Name of the bin
     String name;
@@ -24,7 +26,8 @@ public class GrainBin implements StorageUnit{
      * @param wCapacity Storage Capacity, INTEGER
      */
     public GrainBin(String wName, int wCapacity) {
-
+        this.capacity = wCapacity;
+        this.name = wName;
     }
 
     /**
@@ -41,6 +44,46 @@ public class GrainBin implements StorageUnit{
      */
     public int getCapacity() {
         return this.capacity;
+    }
+
+    /**
+     * Add grain to the bin
+     * @param x integer value describing how much grain is being added
+     * @return integer describes surplus in removal number
+     */
+    public int addGrain(int x) {
+        if (!this.isFull()) {
+            if (this.current + x < this.capacity) {
+                this.current += x;
+                return 0;
+            }
+            else {
+                this.current += this.capacity - this.current;
+                return x - (this.capacity - this.current);
+            }
+        }
+        System.out.print("Nothing added, bin might be full");
+        return 0;
+    }
+
+    /**
+     * Remove grain from bin
+     * @param x integer value for how much to be removed
+     * @return integer describes surplus in removal number
+     */
+    public int removeGrain(int x) {
+        if (!this.isEmpty()) {
+            if (this.current > x) {
+                this.current -= x;
+                return 0;
+            }
+            else {
+                this.current = 0;
+                return this.current;
+            }
+        }
+        System.out.print("Nothing removed, bin might be empty");
+        return 0;
     }
 
     /**
@@ -67,6 +110,11 @@ public class GrainBin implements StorageUnit{
         return this.capacity == this.current;
     }
 
+    public String toString() {
+        return "\nBin name: " + this.name + "\nCurrent Load: " + this.getCurrent() +
+                "\nCapacity: " + this.getCapacity() + "\n";
+    }
+
     /**
      * Sets current storage value to zero or empty
      */
@@ -76,5 +124,84 @@ public class GrainBin implements StorageUnit{
 
      public static void main(String[] args) {
         // Testing
-     }
+         GrainBin w106 = new GrainBin("W106", 5000);
+         System.out.print(w106.toString());
+         if (!w106.isEmpty()) {
+            System.out.print("Bin is returning false but should be true for empty status\n");
+         }
+         if (w106.isFull()) {
+             System.out.print("Bin is returning true but should be false for full status\n");
+         }
+         if (w106.getCurrent() != 0) {
+             System.out.print("Bin status is non-zero upon construction\n");
+         }
+         if (w106.getCapacity() != 5000) {
+             System.out.print("Bin capacity was not constructed correctly.\n");
+         }
+         if (!w106.getName().equals("W106")) {
+             System.out.print("Bin name was not correctly set\n");
+         }
+
+         // non-empty addgrain test
+         w106.addGrain(2000);
+         System.out.print(w106.toString());
+         if (w106.isEmpty()) {
+             System.out.print("Bin is returning true but should be false for empty status\n");
+         }
+         if (w106.isFull()) {
+             System.out.print("Bin is returning true but should be false for full status\n");
+         }
+         if (w106.getCurrent() != 2000) {
+             System.out.print("Bin status is non-zero upon construction\n");
+         }
+
+         // non-empty removegrain test
+         w106.removeGrain(1000);
+         System.out.print(w106.toString());
+         if (w106.isEmpty()) {
+             System.out.print("Bin is returning true but should be false for empty status\n");
+         }
+         if (w106.isFull()) {
+             System.out.print("Bin is returning true but should be false for full status\n");
+         }
+         if (w106.getCurrent() != 1000) {
+             System.out.print("Bin status is non-zero upon construction\n");
+         }
+
+         // empty removeGrain test past zero
+         int excess = w106.removeGrain(4000);
+         System.out.print(w106.toString());
+         if (!w106.isEmpty()) {
+             System.out.print("Bin is returning true but should be false for empty status\n");
+         }
+         if (w106.isFull()) {
+             System.out.print("Bin is returning true but should be false for full status\n");
+         }
+         if (w106.getCurrent() != 0) {
+             System.out.print("Bin status is non-zero upon construction\n");
+         }
+         System.out.print(excess + "\n");
+         if (excess != 1000) {
+             System.out.print("Excess load requirement is wrong\n");
+         }
+
+         // empty addGrain test past capacity
+         w106.addGrain(2500);
+         int leftover = w106.addGrain(4000);
+         System.out.print(w106.toString());
+         if (w106.isEmpty()) {
+             System.out.print("Bin is returning true but should be false for empty status\n");
+         }
+         if (!w106.isFull()) {
+             System.out.print("Bin is returning false but should be true for full status\n");
+         }
+         if (w106.getCurrent() != 5000) {
+             System.out.print("Bin status is non-zero upon construction\n");
+         }
+         System.out.print(leftover + "\n");
+         if (leftover != 1500) {
+             System.out.print("Leftover load requirement is wrong\n");
+         }
+
+    }
 }
